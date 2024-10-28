7c938b63125b81f625b4b87901160266a676ae66
mcenazy159 <73200403+G0d1y@users.noreply.github.com>
2024-10-28 16:52:10 +0330

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -58,8 +58,8 @@ async def download_file(client, url, filename, chat_id, message_id):
         
         for data in response.iter_content(chunk_size=1024):
             if cancel_event.is_set():
+                cancel_event.clear()
                 print("Download cancelled.")
-                os.remove(filename)
                 return None
             
             f.write(data)
